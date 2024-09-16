916838acca0bcccf333e92bb66dd26e3ba94b8f7
IanCist <iantcisternas@gmail.com>
2024-09-16 10:41:08 -0400

    adding welcome screen with java script

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-document.getElementById('welcome-text').style.opacity = 1;
-})
+    const overlay = document.getElementById('overlay');
+    const welcomeText = document.getElementById('welcome-text');
+    const mainContent = document.querySelector('main');
+    const navbar = document.querySelector('nav');
+
+
+    overlay.classList.add('show');
+    setTimeout(() => {
+        mainContent.classList.add('blur-background');
+        navbar.classList.add('blur-background');
+    }, 100); 
+
+ 
+    setTimeout(() => {
+        overlay.classList.remove('show');
+        setTimeout(() => {
+            mainContent.classList.remove('blur-background');
+            navbar.classList.remove('blur-background');
+        }, 2000); 
+    }, 3000); 
+});
