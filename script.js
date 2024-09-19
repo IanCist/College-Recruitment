5971c3a9167aca0d801c938908d14ba18b7b71ec
IanCist <iantcisternas@gmail.com>
2024-09-19 09:12:24 -0400

    added welcome page

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const overlay = document.getElementById('overlay');
     const mainContent = document.querySelector('main');
-})
+    const navbar = document.querySelector('nav');
+
+    overlay.classList.add('show');
+    navbar.classList.add('blur-background');
+
+    setTimeout(() => {
+        overlay.classList.remove('show');
+        mainContent.classList.remove('blur-background');
+        navbar.classList.remove('blur-background');
+    }, 5000);
+    
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+});
