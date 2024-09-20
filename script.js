a05e8b8fa3f273a28e214df6e6f0dfeac757e6c4
IanCist <iantcisternas@gmail.com>
2024-09-20 10:43:38 -0400

    added animated arrow

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const overlay = document.getElementById('overlay');
     const mainContent = document.querySelector('main');
     const navbar = document.querySelector('nav');
+    const arrowContainer = document.querySelector('.arrow-container');
 
     overlay.classList.add('show');
     navbar.classList.add('blur-background');
@@ -11,12 +12,18 @@ document.addEventListener("DOMContentLoaded", function () {
         mainContent.classList.remove('blur-background');
         navbar.classList.remove('blur-background');
     }, 5000);
-    
+
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const arrowContainer = document.querySelector('.arrow-container');
+
+
     window.addEventListener('scroll', function () {
-        if (window.scrollY > 50) {
-            navbar.classList.add('scrolled');
+        if (window.scrollY > 100) {
+            arrowContainer.classList.add('hide');
         } else {
-            navbar.classList.remove('scrolled');
+            arrowContainer.classList.remove('hide');
         }
     });
 });
